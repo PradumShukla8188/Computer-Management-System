b2925f9957fc5edd9ccb753c549d4f5dd4e09e20
PraddumnKumarShineDezign <praddumn.kumar@shinedezign.com>
2026-01-08 13:05:13 +0530

    resolved ai issue

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,7 +1,7 @@
 export async function generateWithGemini(prompt: string) {
     try {
         const res = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API_KEY}`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API_KEY}`,
             {
                 method: 'POST',
                 headers: {
